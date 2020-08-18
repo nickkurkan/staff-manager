d759c62f21d77ead1e0d95d600f81bf01cfeef6d
Nikita Kurkan <kurkan.n.s@gmail.com>
2020-08-18 18:09:28 +0300

    init

---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+
+Vue.use(VueRouter);
+
+const routes = [];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
